e5079ba152d53e7809f772f7f1bc79c2f8073bc5
Matt Wass <mattywass@btinternet.com>
2024-11-24 19:59:05 +0000

    basic js added, functions added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("connected1")
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName()
+})
+
+function getComputerChoice() {
+
+}
+
+function determineWinner() {
+
+}
+
+function updateScore() {
+
+}
+
+function showbothchoices() {
+
+}
+
