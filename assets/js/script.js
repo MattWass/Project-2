963814ebbf8048995a8d8d58271e8bcc317f1604
Matt Wass <mattywass@btinternet.com>
2024-11-24 22:31:10 +0000

    computer choice function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,25 @@
 // Wait for the DOM to finish loading before running the game
 
 
-
+// Function to get the computer's choice
+// This function randomly chooses 0, 1 or 2
         function getComputerChoice() {
-
+            const choices = ["rock", "paper", "scissors"];
+            const randomIndex = Math.floor(Math.random() * 3);
+            return choices[randomIndex];
         }
 
         function determineWinner(userChoice, pcChoice) {
             if (userChoice === pcChoice) {
-                return "It's a tie!";
+                return "It is a draw";
             } else if (
                 (userChoice === "rock" && pcChoice === "scissors") ||
                 (userChoice === "scissors" && pcChoice === "paper") ||
                 (userChoice === "paper" && pcChoice === "rock")
             ) {
-                return "You win!";
+                return "You have won";
             } else {
-                return "You lose!";
+                return "You have lost";
             }
         }
 
